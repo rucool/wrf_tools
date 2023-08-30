5cb71fcf248f6ad7c0e0e9bcbdc305603ef16f50
Lori Garzio <lgarzio@marine.rutgers.edu>
2023-08-30 12:10:57 -0400

    add variable 'TEMP10'
    - add variable TEMP10 to get temperature at 10m to match U10 and V10

--- subset_wrf.py
+++ subset_wrf.py
@@ -74,8 +74,14 @@ def main(args):
     diagnostic_vars['U'] = xr.concat(utemp, dim='height')
     diagnostic_vars['V'] = xr.concat(vtemp, dim='height')
 
-    # Interpolate temperature, include 10m
-    heights.insert(0, 10)
+    # Interpolate temperature at 10m to match U10 and V10
+    interp_temp10 = interplevel(getvar(ncfile, 'temp'), new_z, 10, default_fill(np.float32))
+    interp_temp10 = cf.delete_attr(interp_temp10)
+    interp_temp10 = interp_temp10.drop('level')  # drop the height coordinate
+    del interp_temp10.attrs['_FillValue']
+    primary_vars['TEMP10'] = interp_temp10
+
+    # Interpolate temperature to the same heights as U and V
     interp_temp = interplevel(getvar(ncfile, 'temp'), new_z, heights, default_fill(np.float32))
     interp_temp = cf.delete_attr(interp_temp)
     interp_temp = interp_temp.rename({'level': 'height'})
@@ -187,8 +193,16 @@ def main(args):
     ds['V10'].attrs['valid_min'] = np.float32(-300)
     ds['V10'].attrs['valid_max'] = np.float32(300)
 
+    # Set temperature at 10m attributes
+    ds['TEMP10'].attrs['description'] = 'temperature'
+    ds['TEMP10'].attrs['long_name'] = 'Air Temperature at 10m'
+    ds['TEMP10'].attrs['standard_name'] = 'air_temperature'
+    ds['TEMP10'].attrs['comment'] = 'Interpolated to a height of 10m from native model levels'
+
     # Set temperature attributes
     ds['TEMP'].attrs['description'] = 'temperature'
+    ds['TEMP'].attrs['long_name'] = 'Air Temperature'
+    ds['TEMP'].attrs['standard_name'] = 'air_temperature'
     ds['TEMP'].attrs['comment'] = 'Interpolated to height from native model levels'
 
     # set primary attributes
